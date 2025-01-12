7230ed87b7d5c35eea823cf9ad2170a091adee2e
angelespejo <angelespejo13@gmail.com>
2025-01-12 06:48:32 +0100

    :construction_worker: build(env): change build process

--- .dovenv/build.binarium.js
+++ .dovenv/build.binarium.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'binarium'
 import { join }         from 'node:path'
 
+import core from './const.js'
+
 export default defineConfig( {
-	input  : 'dist/main.mjs',
-	output : join( process.cwd(), '../../build' ),
-	name   : 'sizium',
+	input  : join( core.packagesDir, 'cli/dist/main.mjs' ),
+	output : join( process.cwd(), 'build' ),
+	name   : core.corePkg.name,
 	// nodeOptions : { esbuild: { plugins: false } },
 } )
